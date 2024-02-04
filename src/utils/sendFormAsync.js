b4ef6bf5b6a42a9161a58e26561b06622de96418
Алексей Заикин <hydrombee@gmail.com>
2024-02-04 04:02:16 +0100

    added comments to the sendFormAsync func

--- src/utils/sendFormAsync.js
+++ src/utils/sendFormAsync.js
@@ -3,12 +3,25 @@ import { requestsBroadcast } from './serviceWorkerMessages' // канал для
 
 // отправка заполненной формы
 
+/**
+ * Функция отправки формы на сервер и передачи service-worker'у информации о запросе.
+ *
+ * @param {Array} inputsValue - Все введенные ответы на вопросы вида:   question: 'answer'
+ * @param {Function} setIsFetching - Происходит ли отправка форма
+ * @param {Function} setIsSubmit - Отправлена ли форма
+ * @param {Function} setIsReadonly - Только ли для просмотра форма
+ * @param {Function} setIsSuccess - Успешный ли запрос (dispatch в context)
+ * @param {Function} setShowModal - Отобразить ли модальное окно (dispatch в context)
+ * @returns {void} - Сумма двух чисел.
+ *
+ */
+
 const sendFormAsync = async (inputsValue, setIsFetching, setIsSubmit, setIsReadonly, setIsSuccess, setShowModal) => {
   inputsValue['complTime'] = new Date().toISOString() // добавление поля complTime в объект inputsValue
 
-  setIsFetching(true) // начало запроса на сервер
-  setIsSubmit(true) // произошло нажатие на кнопку [отправить]  (блокируем кнопку)
-  setIsReadonly(true) // устанавливаем для ответов режим чтения
+  setIsFetching(true)
+  setIsSubmit(true)
+  setIsReadonly(true)
 
   // отправляем сообщение в SW о том, что этот запрос не является повторным
   requestsBroadcast.postMessage({
@@ -18,15 +31,15 @@ const sendFormAsync = async (inputsValue, setIsFetching, setIsSubmit, setIsReado
   await checkAPI
     .postData(inputsValue) // отправка данных на сервер
     .then(() => {
-      setIsSuccess(true) // запрос выполнен успешно
+      setIsSuccess(true)
     })
     .catch((error) => {
-      setIsSuccess(false) // запрос не выполнен
+      setIsSuccess(false)
 
       console.error(error)
     })
     .finally(() => {
-      setIsFetching(false) // конец запроса
+      setIsFetching(false)
       setShowModal(true)
     })
 }
