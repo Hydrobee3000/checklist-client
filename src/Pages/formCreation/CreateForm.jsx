33155ef4a9f1a4d98425bc1c073f01fa7b367f13
Алексей Заикин <hydrombee@gmail.com>
2024-02-09 00:07:09 +0100

    added icons to the tools panel, updated styles

--- src/Pages/formCreation/CreateForm.jsx
+++ src/Pages/formCreation/CreateForm.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
-import { Button, Input, Tooltip, Select } from 'antd'
+import { Button, Input, Tooltip, Select, Typography } from 'antd'
 import Header from '../../Components/Header/Header'
-import { CloseOutlined } from '@ant-design/icons'
+import {
+  CloseOutlined,
+  CalendarOutlined,
+  FontSizeOutlined,
+  CheckCircleOutlined,
+  CheckSquareOutlined,
+  FieldNumberOutlined,
+  LineOutlined,
+} from '@ant-design/icons'
 import { questionsTypes } from '../checklists/data_checklists'
 import { TitleEl } from './elements/TitleEl'
 import { QuestionInputEl } from './elements/QuestionInputEl'
@@ -172,75 +180,99 @@ const CreateForm = () => {
           style={{
             display: 'flex',
             flexDirection: 'column',
-            width: '170px',
+            width: '190px',
             border: '1px dashed black',
             borderRadius: '5px',
-            padding: '5px',
-            marginRight: '15px',
+            padding: '10px',
+            marginRight: '25px',
           }}
         >
-          <h5 style={{ marginTop: '10px' }}>Добавить элемент</h5>
-          <Button block style={{ height: '30px' }} onClick={onCreateBlockTitle}>
-            Заголовок
-          </Button>
-
-          <h5 style={{ marginTop: '15px' }}>Добавить вопрос</h5>
+          <Typography level={2} style={{ marginBottom: '10px', opacity: 0.5, fontStyle: 'italic' }}>
+            Добавить элемент
+          </Typography>
           <Button
             block
             size='small'
-            style={{ height: '30px', marginBottom: '8px' }}
+            style={{ height: '30px', paddingRight: '95%' }}
+            onClick={onCreateBlockTitle}
+            icon={<LineOutlined style={{ paddingRight: '5px', opacity: 0.5 }} />}
+          >
+            Заголовок
+          </Button>
+
+          <Typography level={2} style={{ marginBottom: '10px', marginTop: '30px', opacity: 0.5, fontStyle: 'italic' }}>
+            Добавить вопрос
+          </Typography>
+          <Button
+            block
+            size='small'
+            style={{ height: '30px', marginBottom: '8px', paddingRight: '95%' }}
             onClick={() => onCreateBlockInputQuestion('date')}
+            icon={<CalendarOutlined style={{ paddingRight: '5px', opacity: 0.4 }} />}
           >
             Дата
           </Button>
           <Button
             block
             size='small'
-            style={{ height: '30px', marginBottom: '8px' }}
+            style={{ height: '30px', marginBottom: '8px', paddingRight: '95%' }}
             onClick={() => onCreateBlockInputQuestion('text')}
+            icon={<FontSizeOutlined style={{ paddingRight: '5px', opacity: 0.5 }} />}
           >
             Текст
           </Button>
           <Button
             block
             size='small'
-            style={{ height: '30px', marginBottom: '8px' }}
+            style={{ height: '30px', marginBottom: '8px', paddingRight: '95%' }}
             onClick={() => onCreateBlockInputQuestion('number')}
+            icon={<FieldNumberOutlined style={{ paddingRight: '5px', opacity: 0.5 }} />}
           >
             Число
           </Button>
-          <Button block size='small' style={{ height: '30px', marginBottom: '8px' }} onClick={() => onCreateBlockRadioQuestion()}>
+          <Button
+            block
+            size='small'
+            style={{ height: '30px', marginBottom: '8px', paddingRight: '95%' }}
+            onClick={() => onCreateBlockRadioQuestion()}
+            icon={<CheckCircleOutlined style={{ paddingRight: '5px', opacity: 0.5 }} />}
+          >
             Один ответ
           </Button>
           <Button
             block
             size='small'
-            style={{ height: '30px', marginBottom: '8px' }}
+            style={{ height: '30px', marginBottom: '8px', paddingRight: '95%' }}
             onClick={() => onCreateBlockRadioQuestion(true)}
+            icon={<CheckSquareOutlined style={{ paddingRight: '5px', opacity: 0.5 }} />}
           >
             Несколько ответов
           </Button>
           <Button
             block
             size='small'
-            style={{ height: '30px', marginBottom: '8px' }}
+            style={{ height: '30px', marginBottom: '8px', paddingRight: '95%' }}
             onClick={() => onCreateBlockSelectQuestion(false)}
+            icon={<CheckCircleOutlined style={{ paddingRight: '5px', opacity: 0.5 }} />}
           >
-            Выпадающий список
+            Cписок
           </Button>
         </div>
 
         <form className='app__content_form' style={{ flex: 1 }}>
-          <h5>Название формы</h5>
-          <Input
-            size='large'
-            style={{ marginBottom: '40px' }}
-            placeholder='Введите название формы'
-            onChange={(e) => {
-              saveAnswersToState(setInputsValue, 'formsName', e.target.value)
-            }}
-            allowClear
-          />
+          <div style={{ marginRight: '10px', marginBottom: '40px' }}>
+            <Typography level={2} style={{ marginBottom: '10px', fontWeight: 'bold' }}>
+              Название формы
+            </Typography>
+            <Input
+              size='large'
+              placeholder='Введите название формы'
+              onChange={(e) => {
+                saveAnswersToState(setInputsValue, 'formsName', e.target.value)
+              }}
+              allowClear
+            />
+          </div>
 
           {inputsValue.elements.map((question) =>
             question.element.type === 'title' ? (
