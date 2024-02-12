32f18f5bfe7bb237900eac736df70be3b19effdf
Алексей Заикин <hydrombee@gmail.com>
2024-02-12 19:04:11 +0100

    the code of the QuestionRenderer component moved inside Checklist.jsx

--- src/Pages/checklists/Checklists.jsx
+++ src/Pages/checklists/Checklists.jsx
@@ -3,11 +3,65 @@ import { appContext, setIsSuccess, setShowModal } from '../../store/reducers/app
 import { BlockTitle, BlockTitleComment } from '../../Components/FormsParts/Titles/BlockTitle' // заголовок блока вопросов
 import Header from '../../Components/Header/Header'
 import SubmitButton from '../../Components/FormsParts/Buttons/SubmitButton' // кнопка отправки формы
-import QuestionRenderer from '../../utils/QuestionsRender'
+import InputQuestion from '../../Components/FormsParts/Questions/InputQuestion'
+import RadioQuestion from '../../Components/FormsParts/Questions/RadioQuestion'
+import RadioPlusInputQuestion from '../../Components/FormsParts/Questions/RadioPlusInputQuestion'
+import WithAdditionalQuestion from '../../Components/FormsParts/Questions/WithAdditionalQuestion/WithAdditionalQuestion'
 import defaultSetValue from '../../utils/defaultSetValue' // ф-я установки значения по-умолчанию в стейт
 import sendFormAsync from '../../utils/sendFormAsync' // ф-я отправки данных формы на сервер
 
-//
+const QuestionRenderer = ({ questionData, setInputsValue, inputsValue, isReadonly, today }) => {
+  const { component, type, isRequire, isMultipleAnswers } = questionData
+
+  switch (component) {
+    case 'INPUT':
+      return (
+        <InputQuestion
+          dataQuestion={questionData}
+          setInputsValue={setInputsValue}
+          defaultValue={type === 'date' ? today : undefined}
+          isReadonly={isReadonly}
+          required={isRequire}
+        />
+      )
+    case 'RADIO':
+      return (
+        <RadioQuestion
+          dataQuestion={questionData}
+          setInputsValue={setInputsValue}
+          inputsValue={inputsValue}
+          isReadonly={isReadonly}
+          required={isRequire}
+          multiple={isMultipleAnswers}
+          inputType={type}
+        />
+      )
+    case 'RADIO_PLUS_INPUT':
+      return (
+        <RadioPlusInputQuestion
+          dataQuestion={questionData}
+          setInputsValue={setInputsValue}
+          inputsValue={inputsValue}
+          isReadonly={isReadonly}
+          inputType={type}
+          required={isRequire}
+        />
+      )
+    case 'RADIO_WITH_ADDITIONAL_INPUT':
+      return (
+        <WithAdditionalQuestion
+          dataQuestion={questionData}
+          setInputsValue={setInputsValue}
+          inputsValue={inputsValue}
+          isReadonly={isReadonly}
+          required={isRequire}
+        />
+      )
+
+    default:
+      return null
+  }
+}
 
 const Checklist = ({ data }) => {
   const { dispatch } = useContext(appContext)
